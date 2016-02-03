b704e1512834357d3b4354e24721d237c424b71d
Trenton Schulz <tws@norwegianrockcat.com>
2016-02-03 19:05:21 +0100

    An updated mutt script for the årskontingent.
    Deals with some UTF-8 issues.

---
+++ aarskontingent/send-mutt.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# for those that want to send stuff on their Mac. You need to have mutt set up and configured actually.
+
+import sys
+import codecs
+import subprocess
+import os
+
+def main():
+    if len(sys.argv) != 2:
+        sys.stderr.write("I need an ID (email file), exiting.\n")
+        return -1
+
+    sekretariatAddress = "sekretariat@oslopinball.no"
+    os.environ['REPLYTO'] = sekretariatAddress
+    emailText = codecs.open("emails/" + sys.argv[1] + ".txt", 'r', 'utf-8').readlines()
+    header = emailText[:6]
+    subject = header[0][9:].strip()
+    to = header[2][3:].strip()
+    
+    body = emailText[6:]
+    muttBody = codecs.open("emails/tmp.txt", 'w', 'utf-8')
+    muttBody.write("".join(body))
+    muttBody.close()
+    command = u"mutt -c '%s' -s '%s' -- %s < emails/tmp.txt" % (sekretariatAddress, subject, to)
+#    print command
+    os.system(command.encode('utf-8'))
+
+main()
+
+#ID=$1
+#
+#echo "Sending email to: $ID"
+#RECIPIENT=`cat emails/$ID.txt | grep "^To:" | awk '{ print $2 }'`
+#SUBJECT=`cat emails/$ID.txt` | grep "^Subject:" | awk '{ print $2 " " $3 " " $4 " " $5 }'
+#TOTALLINES=`wc -l emails/$ID.txt | awk '{ print $1 }'`
+#BODYLINES=`expr $TOTALLINES - 6`
+#BODY=`tail -$BODYLINES emails/$ID.txt`
+
